7865539def07f2d6bc6007aed2a92c469aaf345c
Fernando Santos <s4antosf3rnando@gmail.com>
2021-10-04 15:08:45 -0300

    pagina de perguntas feita

--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse('pagina home')
+
+def perguntas(request):
+    return HttpResponse('pagina perguntas')
