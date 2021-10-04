94e378e052983a7d7a176b1e07983911858887ee
Fernando Santos <s4antosf3rnando@gmail.com>
2021-10-04 15:20:24 -0300

    capturando dados da home

--- home/views.py
+++ home/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('pagina home')
+    if request.method != 'POST':
+        return render(request, 'home/index.html')
+
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    email = request.POST.get('email')
+    
+    print(nome, sobrenome, email)
+
+    return render(request, 'home/index.html')
 
 def perguntas(request):
     return HttpResponse('pagina perguntas')
