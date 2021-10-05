80e967610e0a1e8bff993e77faf41e6371b17db6
Fernando Santos <s4antosf3rnando@gmail.com>
2021-10-05 15:49:45 -0300

    configurando formulario

--- web/home/views.py
+++ web/home/views.py
@@ -10,11 +10,6 @@ def index(request):
     nome = request.POST.get('nome')
     sobrenome = request.POST.get('sobrenome')
     email = request.POST.get('email')
- 
-    if User.objects.filter(email=email).exists():
-        return render(request, 'home/index.html', {
-            'msg': 'ja existe krl coloca outro'
-        })
 
     user = User(
         nome=nome,
@@ -25,7 +20,7 @@ def index(request):
     user.save()
 
     return render(request, 'home/index.html', {
-        'nome': nome
+        'message': 'Obrigado por se inscrever'
     })
 
 
