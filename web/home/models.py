d46cddb9d5b4f27eb54b897413fdb5771f0f8c78
Fernando Santos <s4antosf3rnando@gmail.com>
2021-10-05 10:40:46 -0300

    alterando models e admin

--- web/home/models.py
+++ web/home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.deletion import DO_NOTHING
+from django.db.models.fields import BLANK_CHOICE_DASH
 
 # Create your models here.
 class User(models.Model):
@@ -8,3 +10,38 @@ class User(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Alternativa(models.Model):
+    a = models.TextField(blank=True)
+    b = models.TextField(blank=True)
+    c = models.TextField(blank=True)
+    d = models.TextField(blank=True)
+    e = models.TextField(blank=True)
+
+class Vestibular(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+
+class Banca(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+
+class Pergunta(models.Model):
+    texto = models.TextField()
+    alternativa_certa = models.CharField(max_length=1)
+    banca = models.ForeignKey(Banca, on_delete=DO_NOTHING)
+    vestibular = models.ForeignKey(Vestibular, on_delete=DO_NOTHING)
+    alternativas = models.ForeignKey(Alternativa, on_delete=DO_NOTHING)
+
+    def __str__(self):
+        return self.texto
+
