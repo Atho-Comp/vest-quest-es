dbc5c2cbf3f3b0d46027901d07f58fce3a19fc0c
Fernando Santos <s4antosf3rnando@gmail.com>
2021-10-15 15:26:22 -0300

    capturando dados formularios da home

--- home/views.py
+++ home/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method != 'POST':
+        return render(request, 'home/index.html')
+
+    name = request.POST.get('name')
+    last_name = request.POST.get('lastname')
+    email = request.POST.get('email')
+
+    print(name, last_name, email)
     return render(request, 'home/index.html')
