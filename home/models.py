9c23e38cb6fe97a7bbbd20c2889f124f4e3935f4
Fernando Santos <s4antosf3rnando@gmail.com>
2021-10-15 15:33:10 -0300

    alterando field model

--- home/models.py
+++ home/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    nome = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
